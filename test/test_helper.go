ca9f5be4ff648df23328dc482727005dd6878be8
Yoshizumi Endo <yendo0206@gmail.com>
2025-01-13 17:12:10 +0900

    Improve test_helper.go

--- test/test_helper.go
+++ test/test_helper.go
@@ -1,13 +1,10 @@
 package test
 
 import (
-	"os"
+	_ "embed"
 	"path"
 	"path/filepath"
 	"runtime"
-	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -18,33 +15,11 @@ const (
 )
 
 var (
-	NotesFile         string
-	ShellBlockFile    string
-	LocationFile      string
-	LocationExtraFile string
-)
-
-func init() {
-	NotesFile = fullPath(notesFile)
-	ShellBlockFile = fullPath(shellBlockFile)
-	LocationFile = fullPath(locationFile)
+	NotesFile         = fullPath(notesFile)
+	ShellBlockFile    = fullPath(shellBlockFile)
+	LocationFile      = fullPath(locationFile)
 	LocationExtraFile = fullPath(locationExtraFile)
-}
-
-// OpenTestNotesFile opens a test notes file.
-func OpenTestNotesFile(t *testing.T, filename string) *os.File {
-	t.Helper()
-
-	file, err := os.Open(filename)
-	require.NoError(t, err)
-
-	t.Cleanup(func() {
-		err := file.Close()
-		require.NoError(t, err)
-	})
-
-	return file
-}
+)
 
 // fullPath returns a full path of test note file.
 func fullPath(filename string) string {
@@ -53,21 +28,10 @@ func fullPath(filename string) string {
 	return filepath.Join(path.Dir(thisFileName), filename)
 }
 
+//go:embed testdata/expected_titles.txt
+var expectedTitles string
+
 // ExpectedTitles returns the titles of the test notes.
 func ExpectedTitles() string {
-	return `title
-Long title and contents have lines
-Regular expression meta chars in the title are ignored $
-Consecutive blank lines are combined into a single line
-same title
-Heading levels and structures are ignored
-Trailing spaces in the title are ignored
-Spaces before the title are ignored
-Headings in fenced code blocks are ignored
-There can be no blank line
-Titles without a space after the # are not recognized
-URL
-command-line
-command-line with $
-`
+	return expectedTitles
 }
